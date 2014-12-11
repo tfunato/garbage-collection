f3cfdd307d646ea67a623ce1285d4ec105d35a22
tfunato <tfunato@gmail.com>
2014-12-11 16:46:13 +0900

    modify

--- go/lerning/fizzbuzz.go
+++ go/lerning/fizzbuzz.go
@@ -1,13 +1,18 @@
 package main
 
+import fmt "fmt"
+
 func main() {
     for i:=0; i < 100; i++ {
         switch {
-            case i % 3 == 0 && i % 5 == 0;
-                os.Stdout.WriteString(s)
-                break
+            case i % 3 == 0 && i % 5 == 0:
+                fmt.Print("Fizz Buzz\n")
             case i % 3 == 0:
+                fmt.Print("Fizz\n")
             case i % 5 == 0:
+                fmt.Print("Buzz\n")
+            default:
+                fmt.Print(i , "\n")
         } 
     }
 }
